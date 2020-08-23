dbaa4f1dd546269121df7cfc31b1703b1c0bb79e
AdioleDivine <adioledivine27@Gmail.com>
2020-08-23 22:31:45 +0100

    fix

---
+++ .dart_tool/flutter_build/83043939be7570bfe8b4b2aa52ddac2a/main.dart
@@ -0,0 +1,10 @@
+import 'dart:ui' as ui;
+
+import 'package:world_hello/main.dart' as entrypoint;
+
+Future<void> main() async {
+  if (true) {
+    await ui.webOnlyInitializePlatform();
+  }
+  entrypoint.main();
+}
